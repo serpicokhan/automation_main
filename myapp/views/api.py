553e00b1d24875fff49784c2df443191e9b28c8a
moeinserpico <81881855+serpicokhan@users.noreply.github.com>
2023-03-07 22:39:26 +0330

    Update api.py

--- myapp/views/api.py
+++ myapp/views/api.py
@@ -36,4 +36,6 @@ def purchase_api_create(request):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        print(serializer.errors)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
